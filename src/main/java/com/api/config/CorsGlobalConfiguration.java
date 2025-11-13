986bdb08bd60f3a962d0e175522fd6d44a5ed4d0
Prateek Kumar <Prateek.kumar949@gmail.com>
2025-11-13 08:47:35 +0530

    Added one endPoint

---
+++ src/main/java/com/api/config/CorsGlobalConfiguration.java
@@ -0,0 +1,26 @@
+package com.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+	@Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("http://localhost:3000","http://localhost:3002","http://localhost:3001")); // ✅ Use this
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
